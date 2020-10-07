d79d658920e65aa1aef01dd5f43c69afbd14600e
randyfortier <randy.fortier@gmail.com>
2020-10-07 09:34:34 -0400

    Added solution to operation counting challenge

--- section001/04b_AlgorithmAnalysis2/OperationCounting.java
+++ section001/04b_AlgorithmAnalysis2/OperationCounting.java
@@ -15,13 +15,12 @@ public class OperationCounting {
     }                                                              // TOTAL: O(n^2) (quadratic)
 
     public static double[] prefixAverage2(double[] elements) {
-        double[] averages = new double[elements.length];
-        double total = 0.0;
-        for (int avgIndex = 0; avgIndex < elements.length; avgIndex++) {
-            total += elements[avgIndex];
-            averages[avgIndex] = total / (avgIndex + 1);
+        double[] averages = new double[elements.length];                    // 4 = O(1)
+        double total = 0.0;                                                 // 2 = O(1)
+        for (int avgIndex = 0; avgIndex < elements.length; avgIndex++) {    // n iterations, 2 + 2n = O(n)
+            total += elements[avgIndex];                                        // 3 = O(1)
+            averages[avgIndex] = total / (avgIndex + 1);                        // 4 = O(1)
         }
-        return averages;
-    }
-
+        return averages;                                                    // 1 = O(1)
+    }                                                         // TOTAL: 4 + 2 + 2 + 2n + 3n + 4n + 1 = 9n + 9 = O(n)
 }
--- section009/04b_AlgorithmAnalysis2/OperationCounting.java
+++ section009/04b_AlgorithmAnalysis2/OperationCounting.java
@@ -22,12 +22,12 @@ public class OperationCounting {
     }                        // TOTAL: 3 + 2 + 2n + 2n + 2 + n + 3n^2/2 + 4n = 7 + 9n + 1.5n^2 = O(n^2)
 
     public static double[] prefixAverage2(double[] elements) {
-        double[] averages = new double[elements.length];
-        double total = 0.0;
-        for (int avgIndex = 0; avgIndex < elements.length; avgIndex++) {
-            total += elements[avgIndex];
-            averages[avgIndex] = total / (avgIndex + 1);
+        double[] averages = new double[elements.length];                    // 4 = O(1)
+        double total = 0.0;                                                 // 2 = O(1)
+        for (int avgIndex = 0; avgIndex < elements.length; avgIndex++) {    // n iterations, 2 + 2n = O(n)
+            total += elements[avgIndex];                                        // 3 = O(1)
+            averages[avgIndex] = total / (avgIndex + 1);                        // 4 = O(1)
         }
-        return averages;
-    }
+        return averages;                                                    // 1 = O(1)
+    }                                                         // TOTAL: 4 + 2 + 2 + 2n + 3n + 4n + 1 = 9n + 9 = O(n)
 }
