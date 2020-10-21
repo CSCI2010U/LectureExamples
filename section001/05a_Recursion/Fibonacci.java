8ab4d7399c8a34e022fc570c9887efe4cc77840a
randyfortier <randy.fortier@gmail.com>
2020-10-21 11:37:27 -0400

    Added linear recursive fibonacci algorithm

--- section001/05a_Recursion/Fibonacci.java
+++ section001/05a_Recursion/Fibonacci.java
@@ -7,4 +7,28 @@ public class Fibonacci {
 
         return binaryFibonacci(n - 1) + binaryFibonacci(n - 2);
     }
+
+    static class FibResult {
+        public int i, j;
+
+        public FibResult(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+    }
+
+    public static FibResult linearFibonacci(int n) {
+        if (n == 1) {
+            return new FibResult(n, 0);
+        } else {
+            FibResult result = linearFibonacci(n - 1);
+            return new FibResult(result.i + result.j, result.i);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int n = 0; n < 10; n++) {
+            System.out.printf("fib(%d) = %d\n", n, binaryFibonacci(n));
+        }
+    }
 }
