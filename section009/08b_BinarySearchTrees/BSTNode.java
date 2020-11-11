b6157158241c4fc12c7a416446a2f867039336e4
ExperimentsInCS <randy.fortier@gmail.com>
2020-11-11 14:02:19 -0500

    Added binary search tree demo (section 009)

--- section009/08b_BinarySearchTrees/BSTNode.java
+++ section009/08b_BinarySearchTrees/BSTNode.java
@@ -23,4 +23,68 @@ public class BSTNode {
             left.print(depth + 1);
         }
     }
+
+    public void insert(int newValue) {
+        if (newValue < this.value) {
+            // add the new element to the left sub-tree
+
+            if (this.left == null) {
+                // there is no left sub-tree, so this new
+                // node will become our left sub-tree
+                BSTNode newNode = new BSTNode();
+                newNode.value = newValue;
+                newNode.left = null;
+                newNode.right = null;
+                newNode.parent = this;
+                this.left = newNode;
+            } else {
+                // we do have a left sub-tree, so insert the element there
+                this.left.insert(newValue);
+            }
+        } else { // newValue >= this.value
+            // add the new element to the right sub-tree
+
+            if (this.right == null) {
+                // there is no right sub-tree, so
+                // add the element as our right sub-tree
+                BSTNode newNode = new BSTNode();
+                newNode.value = newValue;
+                newNode.left = null;
+                newNode.right = null;
+                newNode.parent = this;
+                this.right = newNode;
+            } else {
+                // there is a right sub-tree, so insert the element there
+                this.right.insert(newValue);
+            }
+        }
+    }
+
+    public int getHeight() {
+        int leftHeight = 0;
+        if (this.left != null) {
+            leftHeight = this.left.getHeight();
+        }
+
+        int rightHeight = 0;
+        if (this.right != null) {
+            rightHeight = this.right.getHeight();
+        }
+
+        return (leftHeight > rightHeight) ? leftHeight + 1 : rightHeight + 1;
+    }
+
+    public int getBalance() {
+        int leftHeight = 0;
+        if (this.left != null) {
+            leftHeight = this.left.getHeight();
+        }
+
+        int rightHeight = 0;
+        if (this.right != null) {
+            rightHeight = this.right.getHeight();
+        }
+        
+        return leftHeight - rightHeight;
+    }
 }
--- section009/08b_BinarySearchTrees/BinarySearchTree.java
+++ section009/08b_BinarySearchTrees/BinarySearchTree.java
@@ -5,6 +5,8 @@ public class BinarySearchTree {
         if (root == null) {
             System.out.println("null");
         } else {
+            System.out.printf("Height:  %d\n", root.getHeight());
+            System.out.printf("Balance: %d\n", root.getBalance());
             root.print(0);
         }
     }
@@ -19,16 +21,34 @@ public class BinarySearchTree {
 
             root = newNode;
         } else {
-            // TODO: Implement insert
+            root.insert(newValue);
         }
     }
 
+    /*
+                        10
+                5               15
+            2       7       12      20
+    */
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
 
-        tree.insert(7);
-        tree.insert(3);
+        // tree.insert(2);
+        // tree.insert(5);
+        // tree.insert(7);
+        // tree.insert(10);
+        // tree.insert(12);
+        // tree.insert(15);
+        // tree.insert(20);
+
         tree.insert(10);
+        tree.insert(5);
+        tree.insert(15);
+        tree.insert(2);
+        tree.insert(7);
+        tree.insert(12);
+        tree.insert(20);
+        tree.insert(11);
 
         tree.printInOrder();
     }
